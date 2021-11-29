9ffa4849a1a17edc6567e4d2472ef4c5bf91bb04
AaronJH21 <93291172+AaronJH21@users.noreply.github.com>
2021-11-29 17:20:08 -0300

    Add files via upload

---
+++ javascrip ciclos/script.js
@@ -0,0 +1,8 @@
+let i = 0
+
+do {
+    let nombre = prompt("ingrese el nombre de los alumnos")
+    let apellido = prompt("ingrese el apellido de los alumnos")
+    console.log(" su nombre es:" +nombre+" su apellido es "+apellido)
+    i++;
+} while (i < 4);
